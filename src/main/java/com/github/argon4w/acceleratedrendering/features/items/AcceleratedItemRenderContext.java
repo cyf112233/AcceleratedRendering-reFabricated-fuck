3e7b79599af4df07476ad1ddaf68307ce6c38793
ZhuRuoLing <jkl-963@live.com>
2025-03-24 03:14:29 +0800

    sync from upstream

--- src/main/java/com/github/argon4w/acceleratedrendering/features/items/AcceleratedItemRenderContext.java
+++ src/main/java/com/github/argon4w/acceleratedrendering/features/items/AcceleratedItemRenderContext.java
@@ -1,14 +1,15 @@
 package com.github.argon4w.acceleratedrendering.features.items;
 
+import com.github.argon4w.acceleratedrendering.core.mixins.MCItemColorsAccessor;
 import com.github.argon4w.acceleratedrendering.features.items.mixins.ItemColorsAccessor;
 import lombok.Getter;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.color.item.ItemColor;
 import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.item.ItemStack;
 
-@Getter
 public class AcceleratedItemRenderContext {
 
     private final ItemStack itemStack;
@@ -22,8 +23,16 @@ public class AcceleratedItemRenderContext {
             RandomSource random
     ) {
         this.itemStack = itemStack;
-        this.itemColor = ((ItemColorsAccessor) Minecraft.getInstance().getItemColors()).getItemColors().getOrDefault(itemStack.getItem(), EmptyItemColor.INSTANCE);
+        this.itemColor = getItemColorOrDefault(itemStack);
         this.bakedModel = bakedModel;
         this.random = random;
+    }
+
+    private static ItemColor getItemColorOrDefault(ItemStack itemStack) {
+        MCItemColorsAccessor accessor = (MCItemColorsAccessor) Minecraft.getInstance();
+        int id = BuiltInRegistries.ITEM.getId(itemStack.getItem());
+        ItemColor color = ((ItemColorsAccessor) (accessor.getItemColors())).getItemColors().byId(id);
+        if (color == null) return EmptyItemColor.INSTANCE;
+        return color;
     }
 }
